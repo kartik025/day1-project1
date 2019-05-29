80c6ab44af61e8dd194ba2a2ffc623612876c1d4
kartik <k1997.suryawanshi@gmail.com>
2019-05-29 18:19:04 +0530

    confict made by kartik

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const productrouter = require('./product');
 const app = express();
 
-app.use(productrouter);
+const router  = require('express');
 app.listen(3000,'0.0.0.0',()=>{
     console.log('server started on port 3000');
 })
