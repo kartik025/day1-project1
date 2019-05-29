465cfe5758a4e6444890c7ff751f72237100eb2e
spawade99 <sspawade333@gmail.com>
2019-05-29 18:26:59 +0530

    conflit try 2

--- server.js
+++ server.js
@@ -2,7 +2,9 @@ const express = require('express');
 const productrouter = require('./product');
 const app = express();
 
+const v = express('express');
 app.use(productrouter);
+app.unsubscribe(productrouter);
 app.listen(3000,'0.0.0.0',()=>{
     console.log('server started on port 3000');
 })
